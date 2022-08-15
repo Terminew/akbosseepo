207c373c75a40240a65581b69e7ce52c4d8be81d
archon0p <107740668+archon0p@users.noreply.github.com>
2022-08-15 15:00:59 +0530

    Update tg_upload_status.py

--- bot/helper/mirror_utils/status_utils/tg_upload_status.py
+++ bot/helper/mirror_utils/status_utils/tg_upload_status.py
@@ -1,6 +1,4 @@
 from bot.helper.ext_utils.bot_utils import MirrorStatus, EngineStatus, get_readable_file_size, get_readable_time
-from bot import DOWNLOAD_DIR
-
 
 class TgUploadStatus:
     def __init__(self, obj, size, gid, listener):
@@ -18,44 +16,37 @@ class TgUploadStatus:
 
     def size_raw(self):
         return self.__size
-
     def size(self):
         return get_readable_file_size(self.__size)
 
     def status(self):
         return MirrorStatus.STATUS_UPLOADING
-    
+
     def eng(self):
         return EngineStatus.STATUS_TG
 
     def name(self):
         return self.__obj.name
-
     def progress_raw(self):
         try:
             return self.__obj.uploaded_bytes / self.__size * 100
         except ZeroDivisionError:
             return 0
-
     def progress(self):
         return f'{round(self.progress_raw(), 2)}%'
-
     def speed_raw(self):
         """
         :return: Upload speed in Bytes/Seconds
         """
         return self.__obj.speed
-
     def speed(self):
         return f'{get_readable_file_size(self.speed_raw())}/s'
-
     def eta(self):
         try:
             seconds = (self.__size - self.__obj.uploaded_bytes) / self.speed_raw()
             return f'{get_readable_time(seconds)}'
         except ZeroDivisionError:
             return '-'
-
     def gid(self) -> str:
         return self.__gid
 
